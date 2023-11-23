55f6e328c98a818c064c2282471922fa53b1fd15
Emil Yosifov <yosifovemil@gmail.com>
2023-11-23 11:28:45 +0200

    waitress

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from views import views
+from waitress import serve
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    serve(app, host='0.0.0.0', port=80, threads=4)
