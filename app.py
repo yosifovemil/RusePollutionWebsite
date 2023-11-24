e9f62bfaa5ecea0cce7a9116f28ae5580d9df692
Emil Yosifov <yosifovemil@gmail.com>
2023-11-24 10:11:25 +0200

    TEMPLATES_AUTO_RELOAD

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from waitress import serve
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=80, threads=4)
